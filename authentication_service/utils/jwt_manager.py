ed8d88fcebfff7c020ab4e9508ef1dd2949e98ec
soumit1803063 <somudasctgbn@gmail.com>
2024-11-22 17:28:38 +0600

    add jwt create access token

---
+++ authentication_service/utils/jwt_manager.py
@@ -0,0 +1,12 @@
+import jwt
+from datetime import datetime, timedelta, timezone
+from ..config import SECRET_KEY,ACCESS_TOKEN_EXPIRE_MINUTES,ALGORITHM
+
+
+def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
